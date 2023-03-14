8ecc34c6e75c42d33c1d7f98ef74015a6c7cc10d
Sebastiaan la Fleur <sebastiaan.lafleur@tno.nl>
2023-03-14 14:52:31 +0100

    Fix coverage report call and add pytest timer to time unit tests.

--- backend/ci/test_unit.sh
+++ backend/ci/test_unit.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 source .venv/bin/activate
-python -m pytest --cov=ev_flex_metric --cov-report=html:./unit_test_coverage/ -v
+python -m pytest --cov=s2_analyzer_backend --cov-report=html:./unit_test_coverage/ -v
