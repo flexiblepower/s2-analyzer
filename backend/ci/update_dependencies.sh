385dde12a154b05d1986e76ce4b5f5d53da0b5f9
Sebastiaan la Fleur <sebastiaan.lafleur@tno.nl>
2023-03-31 17:08:44 +0200

    11: Activate venv before updating dependencies so piptools is available.

--- backend/ci/update_dependencies.sh
+++ backend/ci/update_dependencies.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
+. .venv/bin/activate
 python -m piptools compile -o ./requirements.txt ./requirements.in
 python -m piptools compile -o ./dev-requirements.txt ./dev-requirements.in
