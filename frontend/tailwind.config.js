93352a8084d23075252f5542883a57ae64774d09
sertacafsari <s.b.afsari@student.rug.nl>
2024-03-18 18:17:37 +0100

    NavBar is created. Config files for TailwindCSS is added.

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [ './src/**/*.{js,jsx,ts,tsx}', './index.html',
+],
+  theme: {
+    extend: {
+      colors:{
+        "tno-blue": "#1A0DAB",
+        "stephanie-color": "#101014",
+      }
+    },
+  },
+  plugins: [],
+}
+
