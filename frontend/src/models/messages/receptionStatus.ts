cbfc33e6c3f0d86b8e9e62757a92a574ddb00fa8
panayiotispanagi <p.panayi.1@student.rug.nl>
2024-03-28 03:23:41 +0100

    removed cyclic dependency between ReceptionStatus - MessageHeader

--- frontend/src/models/messages/receptionStatus.ts
+++ frontend/src/models/messages/receptionStatus.ts
@@ -1,7 +1,11 @@
-import MessageHeader from "./messageHeader"
 import {ReceptionStatusValues} from "../dataStructures/receptionStatusValues.ts";
 
-export default interface ReceptionStatus extends MessageHeader{
+export default interface ReceptionStatus {
+    time: Date
+    sender: string | null
+    receiver: string | null
+    message_type: string
+    message_id: string | null
     subject_message_id: string
     status: ReceptionStatusValues
     diagnostic_label: string | null
