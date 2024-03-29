23f79add8cd8330ea442131eaa12c57629a8f0f7
sertacafsari <s.b.afsari@student.rug.nl>
2024-03-29 12:29:10 +0100

    Changes in Filters

--- frontend/src/components/navbar/NavBar.tsx
+++ frontend/src/components/navbar/NavBar.tsx
@@ -3,25 +3,27 @@ import tnologo from "../../assets/TNO-logo.svg";
 import s2logo from "../../assets/s2-analyzer-logo.png";
 import Terminal from "./navbar_items/terminal/Terminal";
 import OptionsMenu from "./navbar_items/optionsmenu/OptionsMenu";
-import {parser} from '../../parser/Parser.ts';
+import { parser } from "../../parser/Parser.ts";
 import MessageHeader from "../../models/messages/messageHeader.ts";
+import { Filters } from "../../models/filters.ts";
 
-interface Props {
-  messages: React.Dispatch<React.SetStateAction<MessageHeader[]>>,
-  filter:  React.Dispatch<React.SetStateAction<(m: MessageHeader) => boolean>>
+interface NavBarProps {
+  messages: React.Dispatch<React.SetStateAction<MessageHeader[]>>;
+  filters: Filters
+  onFilterChange: (filters: Filters) => void;
 }
 
 /**
  * The component for the Navigation Bar
  * @returns the Navigation Bar
  */
-function NavBar(props:Props) {
+function NavBar({messages, filters, onFilterChange}: NavBarProps) {
   const [isVisibleTerminal, setIsVisibleTerminal] = useState(false);
   const [isVisibleOptions, setIsVisibleOptions] = useState(false);
 
   const getFiles = async () => {
-    props.messages(await parser.parseLogFile());
-  }
+    messages(await parser.parseLogFile());
+  };
 
   return (
     <>
@@ -31,7 +33,7 @@ function NavBar(props:Props) {
             <div className="flex items-center justify-center sm:items-stretch sm:justify-end">
               <div className="hidden sm:ml-6 sm:block">
                 <div className="flex space-x-4 mr-10">
-                  <img src={tnologo} alt="TNO logo"/>
+                  <img src={tnologo} alt="TNO logo" />
                 </div>
               </div>
               <div className="mx-8 text-white font-semibold font-sans text-3xl cursor-pointer select-none hover:scale-110">
@@ -46,20 +48,20 @@ function NavBar(props:Props) {
               </div>
               <div className="mx-8 text-white font-semibold font-sans text-3xl cursor-pointer select-none hover:scale-110">
                 <div
-                    onClick={() => setIsVisibleOptions(!isVisibleOptions)}
-                    className="clickable-heading"
+                  onClick={() => setIsVisibleOptions(!isVisibleOptions)}
+                  className="clickable-heading"
                 >
-                  <OptionsMenu filter={props.filter}/>
+                  <OptionsMenu filters={filters} onFilterChange={onFilterChange}/>
                 </div>
               </div>
             </div>
           </div>
           <div className="absolute right-0 top-0 flex items-center pr-6">
-            <img src={s2logo} alt="S2-Analyzer Logo" className="h-24"/>
+            <img src={s2logo} alt="S2-Analyzer Logo" className="h-24" />
           </div>
         </div>
       </nav>
-      {isVisibleTerminal && <Terminal lines={parser.getLines()}/>}
+      {isVisibleTerminal && <Terminal lines={parser.getLines()} />}
     </>
   );
 }
---
+++ frontend/src/models/filters.ts
@@ -0,0 +1,10 @@
+type Filters = {
+    CEM: boolean;
+    RM: boolean;
+    min: number | null;
+    max: number | null;
+    logs: boolean;
+    warnings: boolean;
+  };
+
+  export type { Filters };
