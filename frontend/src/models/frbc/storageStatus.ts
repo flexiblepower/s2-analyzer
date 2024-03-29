edab63ba5dd18390a6aa5c621d0adff94d08571f
sertacafsari <131170576+sertacafsari@users.noreply.github.com>
2024-03-29 16:37:17 +0100

    Delete frontend directory

--- frontend/src/models/frbc/storageStatus.ts
+++
@@ -1,5 +0,0 @@
-import MessageHeader from "../messageHeader";
-
-export default interface StorageStatus extends MessageHeader{
-    present_fill_level: number
-}
--- frontend/src/models/revokeObject.ts
+++
@@ -1,7 +0,0 @@
-import MessageHeader from "./messageHeader"
-import {RevokableObjects} from "./dataStructures/revokableObjects.ts";
-
-export default interface RevokeObject extends MessageHeader{
-    object_type: RevokableObjects
-    object_id: string
-}
