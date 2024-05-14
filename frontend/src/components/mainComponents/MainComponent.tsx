18dc460d3a6ceeff9a4c6e2cf6024487c9028997
steph-maya <s.m.radhakrishnan@student.rug.nl>
2024-05-15 01:41:02 +0200

    fixed

--- frontend/src/components/mainComponents/MainComponent.tsx
+++ frontend/src/components/mainComponents/MainComponent.tsx
@@ -1,4 +1,5 @@
 import cemBox from '../../assets/cemBox.png';
+import rmBox from '../../assets/rmBox.png';
 import MessageList from '../messages/MessageList';
 import MessageHeader from '../../models/messages/messageHeader';
 import DeviceBox from '../devices/DeviceBox';
@@ -10,9 +11,10 @@ interface props<T extends MessageHeader> {
 function MyComponent<T extends MessageHeader>(props: props<T>) {
   return (
     <div className="max-w-xl h-5/6 rounded-xl shadow-[0_1px_30px_1px_rgba(0,0,0,0.3)] grid grid-cols-12 grid-rows-12">
-      <DeviceBox title={'CEM'} c_start={2} c_end={4} r_start={11} l_start={3}/>
-      <DeviceBox title={'RM'} c_start={10} c_end={12} r_start={11} l_start={11}/>
-      <div className='col-start-3 col-end-11 row-start-1 row-end-11' style={{overflow: "auto" }}>
+      <DeviceBox title={"CEM"} c_start={2} c_end={4} r_start={11} l_start={3}/>
+      <DeviceBox title={"RM"} c_start={10} c_end={12} r_start={11} l_start={11}/>
+
+      <div className='col-start-3 col-end-11 row-start-2 row-end-11' style={{overflow: "auto" }}>
         <MessageList<MessageHeader> messages={props.searchedMessages}></MessageList>
       </div>
 
