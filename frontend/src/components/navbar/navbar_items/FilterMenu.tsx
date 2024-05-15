ab4d28812f13a5e1850ee242879389b87e40ac9e
sertacafsari <s.b.afsari@student.rug.nl>
2024-05-15 17:32:40 +0200

    The closing property of the Filter Menu is fixed.

--- frontend/src/components/navbar/navbar_items/FilterMenu.tsx
+++ frontend/src/components/navbar/navbar_items/FilterMenu.tsx
@@ -43,7 +43,7 @@ function FilterMenu({ filters, onFilterChange, isVisible }: FilterMenuProps) {
 
   return (
     <div className={`text-left ${isVisible ? "block" : "hidden"}`}>
-      <div className="absolute mt-2 w-60 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5">
+      <div className="absolute mt-2 w-60 origin-top-right rounded-md bg-base-gray shadow-lg ring-1 ring-black/5">
         <div className="py-4 px-4">
           {Object.entries(filters)
             .slice(0, 4)
@@ -54,9 +54,9 @@ function FilterMenu({ filters, onFilterChange, isVisible }: FilterMenuProps) {
                   id={key}
                   checked={value}
                   onChange={() => handleCheckboxChange(key as keyof Filters)}
-                  className="mr-2 size-4"
+                  className="mr-2 size-4 accent-white"
                 />
-                <label htmlFor={key} className="text-lg text-gray-700">
+                <label htmlFor={key} className="text-lg text-white">
                   {key}
                 </label>
               </div>
@@ -64,22 +64,24 @@ function FilterMenu({ filters, onFilterChange, isVisible }: FilterMenuProps) {
           {/*The Type Filter Menu*/}
           <div className="flex items-center justify-between mb-1">
             <button
-              className="flex items-center ml-6 w-auto text-lg text-gray-700"
+              className="flex items-center ml-6 w-auto text-lg text-white"
               onClick={handleTypeFilterVisibility}
             >
               Type Filters
               <svg
-                xmlns="http://www.w3.org/2000/svg"
+                className={`w-3 h-3 transform transition-transform duration-300 ease-in-out ${
+                  isVisibleTypeFilters ? "rotate-180" : "rotate-90"
+                } ml-7`}
+                aria-hidden="true"
                 fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-4 h-4 relative ml-14"
+                viewBox="0 0 10 6"
               >
                 <path
+                  stroke="currentColor"
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5"
+                  strokeWidth="2"
+                  d="M9 5 5 1 1 5"
                 />
               </svg>
             </button>
@@ -90,7 +92,7 @@ function FilterMenu({ filters, onFilterChange, isVisible }: FilterMenuProps) {
             }`}
           >
             {Object.entries(filters)
-              .slice(4,15)
+              .slice(4, 15)
               .map(([key, value]) => (
                 <div className="flex items-center" key={key}>
                   <input
@@ -98,9 +100,9 @@ function FilterMenu({ filters, onFilterChange, isVisible }: FilterMenuProps) {
                     id={key}
                     checked={value}
                     onChange={() => handleCheckboxChange(key as keyof Filters)}
-                    className="mr-2 size-4"
+                    className="mr-2 size-4 accent-white"
                   />
-                  <label htmlFor={key} className="text-gray-700 text-sm">
+                  <label htmlFor={key} className="text-white text-sm">
                     {key}
                   </label>
                 </div>
@@ -109,22 +111,24 @@ function FilterMenu({ filters, onFilterChange, isVisible }: FilterMenuProps) {
           {/*The FRBC Filter Menu*/}
           <div className="flex items-center justify-between mb-1">
             <button
-              className="flex items-center ml-6 w-auto text-lg text-gray-700"
+              className="flex items-center ml-6 w-auto text-lg text-white"
               onClick={handleFRBCFilterVisibility}
             >
               FRBC Filters
               <svg
-                xmlns="http://www.w3.org/2000/svg"
+                className={`w-3 h-3 transform transition-transform duration-300 ease-in-out ${
+                  isVisibleFRBCFilters ? "rotate-180" : "rotate-90"
+                } ml-6`}
+                aria-hidden="true"
                 fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-4 h-4 relative ml-14"
+                viewBox="0 0 10 6"
               >
                 <path
+                  stroke="currentColor"
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5"
+                  strokeWidth="2"
+                  d="M9 5 5 1 1 5"
                 />
               </svg>
             </button>
@@ -143,9 +147,9 @@ function FilterMenu({ filters, onFilterChange, isVisible }: FilterMenuProps) {
                     id={key}
                     checked={value}
                     onChange={() => handleCheckboxChange(key as keyof Filters)}
-                    className="mr-2 size-4"
+                    className="mr-2 size-4 accent-white"
                   />
-                  <label htmlFor={key} className="text-gray-700 text-sm">
+                  <label htmlFor={key} className="text-white text-sm">
                     {key}
                   </label>
                 </div>
