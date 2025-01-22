64d3c1aa8f797613bc31e2ecafbc478ddbf5ca58
Andrew Rutherfoord <andrew.rutherfoord@live.com>
2025-01-22 13:57:04 +0100

    Fixed bug which caused backend crash
    Caused by removing the history path from the config file since we no
    longer need it. However the field was stil in the config reader.

--- backend/s2_analyzer_backend/config.py
+++ backend/s2_analyzer_backend/config.py
@@ -17,25 +17,12 @@ class ModelConfig:
 class Config(YAMLWizard):
     http_listen_address: str
     http_port: int
-    connection_histories_dir: str
-
-    @property
-    def connection_histories_dir_path(self):
-        return Path(self.connection_histories_dir)
 
 
 def read_s2_analyzer_conf() -> Config:
     print(f"Reading configuration at {S2_ANALYZER_CONF}")
     result = Config.from_yaml_file(S2_ANALYZER_CONF)
 
-    if not result.connection_histories_dir_path.exists():
-        result.connection_histories_dir_path.mkdir(parents=True, exist_ok=True)
-
-    if not result.connection_histories_dir_path.is_dir():
-        raise RuntimeError(
-            f"Connection histories location ({result.connection_histories_dir}) should be a directory "
-            f"and it isn't."
-        )
     return result
 
 
